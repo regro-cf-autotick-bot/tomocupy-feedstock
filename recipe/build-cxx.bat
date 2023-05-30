386c037627f5e71a847f1ad381235b7c154a88f6
vnikitin@anl.gov <vnikitin@anl.gov>
2023-05-30 12:11:04 -0500

    fix spelling

--- recipe/build-cxx.bat
+++ recipe/build-cxx.bat
@@ -1,3 +1,5 @@
 :: Set CUDACXX with call to 'where nvcc'
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
 echo %CUDACXX%
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
