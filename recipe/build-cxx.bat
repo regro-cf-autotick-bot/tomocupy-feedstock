32d48afe33ae044f40539eaadf1ac83e7599aa65
vnikitin@anl.gov <vnikitin@anl.gov>
2023-05-30 13:29:35 -0500

    test windows

--- recipe/build-cxx.bat
+++
@@ -1,6 +0,0 @@
-:: Set CUDACXX with call to 'where nvcc'
-
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
-echo %CUDACXX%
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
