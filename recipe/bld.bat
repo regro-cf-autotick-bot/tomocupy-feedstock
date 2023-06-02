4cb4f8c1f2508d0791f8c485d76fecc0be4f7726
vnikitin@anl.gov <vnikitin@anl.gov>
2023-06-02 07:37:25 -0500

    update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-#for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")  
+for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")  
 echo nvcc is %CUDACXX%   
 echo CUDA path is %CUDA_PATH%   
 echo CUDAToolkit path is %CudaToolkitDir%
@@ -12,10 +12,11 @@ set CMAKE_CUDA_COMPILER_TOOLKIT_ROOT=%CudaToolkitDir%
 echo CMAKE_CUDA_COMPILER is %CMAKE_CUDA_COMPILER%
 echo CMAKE_CUDA_COMPILER_TOOLKIT_ROOT is %CMAKE_CUDA_COMPILER_TOOLKIT_ROOT%
 set CMAKE_CUDA_COMPILER_TOOLKIT_ROOT="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2"
+set CUDATOOLKITDIR = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2"
 
 IF EXIST "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2" ECHO C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2 exists.
-IF EXIST "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\nvvm ECHO" C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\nvvm exists.
-IF EXIST "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\nvvm\libdevice ECHO" C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\nvvm\libdevice exists.
+IF EXIST "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\nvvm" ECHO C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\nvvm exists.
+IF EXIST "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\nvvm\libdevice" ECHO C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\nvvm\libdevice exists.
 
 %PYTHON% -m pip install . -vv --no-build-isolation --no-deps
 
