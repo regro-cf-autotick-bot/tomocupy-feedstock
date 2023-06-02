3314c4874cf2a1ab474a71d3bf0bc76b61051d2e
vnikitin@anl.gov <vnikitin@anl.gov>
2023-06-02 11:22:36 -0500

    update bld

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
 for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")  
+set CUDACXX="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\bin\nvcc.exe"
 echo nvcc is %CUDACXX%   
 echo CUDA path is %CUDA_PATH%   
 echo CUDAToolkit path is %CudaToolkitDir%
@@ -11,8 +12,8 @@ set CMAKE_CUDA_COMPILER_TOOLKIT_ROOT=%CudaToolkitDir%
 
 echo CMAKE_CUDA_COMPILER is %CMAKE_CUDA_COMPILER%
 echo CMAKE_CUDA_COMPILER_TOOLKIT_ROOT is %CMAKE_CUDA_COMPILER_TOOLKIT_ROOT%
-type %CUDACXX%
+set "NVCC=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\bin\nvcc.exe --use-local-env"  
 IF EXISTS "%CMAKE_CUDA_COMPILER_TOOLKIT_ROOT%/nvvm/libdevice" echo "%CMAKE_CUDA_COMPILER_TOOLKIT_ROOT%/nvvm/libdevice" exist
 
 
-%PYTHON% -m pip install . -vv --no-build-isolation --no-deps
+%PYTHON% -m pip install . -vv --no-deps
