8a5c530447083e4d3609c7b9388be6861714f19c
vnikitin@anl.gov <vnikitin@anl.gov>
2023-06-02 15:43:48 -0500

    upadte bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,9 +14,6 @@ echo CMAKE_CUDA_COMPILER_TOOLKIT_ROOT is %CMAKE_CUDA_COMPILER_TOOLKIT_ROOT%
 set CUDAToolkit_ROOT=%CudaToolkitDir%
 set CMAKE_CUDA_COMPILER=%CUDACXX%
 set CMAKE_CUDA_COMPILER_TOOLKIT_ROOT=%CudaToolkitDir%
-set CMAKE_CUDA_FLAGS_INIT="-std=c++17"
-set CMAKE_CUDA_STANDARD=17
-set CMAKE_CUDA_STANDARD_REQUIRED=TRUE
 
 if exist "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin/nvcc.exe" echo "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin/nvcc.exe exist" 
 
