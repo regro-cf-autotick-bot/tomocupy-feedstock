4c9a6cacc5fd44f330c1bd07f2ab8b7e82c20651
vnikitin@anl.gov <vnikitin@anl.gov>
2023-06-02 14:40:05 -0500

    update bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -18,5 +18,6 @@ set CMAKE_CUDA_FLAGS_INIT="-std=c++17"
 set CMAKE_CUDA_STANDARD=17
 set CMAKE_CUDA_STANDARD_REQUIRED=TRUE
 
+if exist "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin/nvcc.exe" echo "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin/nvcc.exe exist" 
 
 %PYTHON% -m pip install . -vv --no-build-isolation --no-deps
