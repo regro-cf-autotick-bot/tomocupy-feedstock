5b63a152aa0a8ce80e1583a648a35ab57c4fe428
vnikitin@anl.gov <vnikitin@anl.gov>
2023-06-02 12:19:09 -0500

    update bld

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,7 +15,5 @@ set CMAKE_CUDA_STANDARD_REQUIRED=TRUE
 echo CMAKE_CUDA_COMPILER is %CMAKE_CUDA_COMPILER%
 echo CMAKE_CUDA_COMPILER_TOOLKIT_ROOT is %CMAKE_CUDA_COMPILER_TOOLKIT_ROOT%
 set "NVCC=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\bin\nvcc.exe --use-local-env"  
-IF EXISTS "%CMAKE_CUDA_COMPILER_TOOLKIT_ROOT%/nvvm/libdevice" echo "%CMAKE_CUDA_COMPILER_TOOLKIT_ROOT%/nvvm/libdevice" exist
-
 
 %PYTHON% -m pip install . -vv --no-deps
