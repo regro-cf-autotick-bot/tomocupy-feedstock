9d97102081fd09039b4341aade2688d5cd0979e2
Daniel Ching <carterbox@users.noreply.github.com>
2023-06-03 21:02:56 -0500

    No build script; use Ninja only for Unix

--- recipe/bld.bat
+++
@@ -1,9 +0,0 @@
-:: Set CUDACXX with call to 'where nvcc'
-for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
-
-echo nvcc is %CUDACXX%
-echo CUDA path is %CUDA_PATH%
-echo CMAKE_ARGS is %CMAKE_ARGS%
-
-%PYTHON% -m pip install . -vvv --no-deps --no-build-isolation
-if errorlevel 1 exit /b 1
